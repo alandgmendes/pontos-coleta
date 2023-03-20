c68c09ad472d3094348010433f45c8968bea8d88
alandgmendes <adgmendes@gmail.com>
2023-03-20 00:22:23 -0300

    fixed misisng container

--- src/index.js
+++ src/index.js
@@ -2,10 +2,9 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App";
-
 import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter } from "react-router-dom";
-
+const container = document.getElementById("root");
 const root = createRoot(container);
 root.render(
   <React.StrictMode>
