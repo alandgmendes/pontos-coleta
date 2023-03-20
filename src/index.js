c32d4abe1fe9931fc80167458414c0c5138dc817
alandgmendes <adgmendes@gmail.com>
2023-03-20 00:18:44 -0300

    fixed reactdom

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App";
+
 import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter } from "react-router-dom";
 
-ReactDOM.render(
+const root = createRoot(container);
+root.render(
   <React.StrictMode>
     <BrowserRouter>
       <App />
